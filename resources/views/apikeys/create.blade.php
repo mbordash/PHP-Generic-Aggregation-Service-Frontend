732e8b4a47f7a6fb1babd72a562e3cd5bae6082f
Michael Bordash <michael@internetdj.com>
2015-05-20 18:38:20 -0400

    Updated query api docs

--- resources/views/apikeys/create.blade.php
+++ resources/views/apikeys/create.blade.php
@@ -5,6 +5,10 @@
 
             <h2>Create App & Generate Token</h2>
 
+            <p>Our service was built for apps "in the wild". If you absolutely need to PUT/GET data via JavaScript and you embed your API token in your
+            web page, people will be able to PUT/GET data using your API token in a way that you may not have intended. A better approach would be to create a
+            server side script to proxy requests for your JavaScript methods. In this way, you will keep your API token private and secure.</p>
+
             {!! Form::model(new App\Apikey, ['route' => ['apikeys.store']]) !!}
 
             @include('apikeys/partials/_form', ['submit_text' => 'Create App & Token'])
